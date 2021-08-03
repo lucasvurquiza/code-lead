c1f85d0bf425e3cca47df94b7718cd8181216456
Lucas Urquiza <urquizaxd@gmail.com>
2021-08-03 11:32:39 -0300

    Refatorando a pasta public

--- src/components/Signup/index.tsx
+++ src/components/Signup/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './styles.css';
 
-export const Login = () => {
+export const Signup = () => {
   return (
     <>
-      <div className='divLogin'>
+      <div className='divSignup'>
         <h1>Welcome to CodeLeap network!</h1>
         <h3>Please enter your username</h3>
         <input type="text" placeholder={'John doe'}/>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Login } from '../components/Login';
+import { Signup } from '../components/Signup';
 import './styles.css';
 
 function App() {
   return (
-    <div className='modalLogin'>
-      <Login />
+    <div className='modalSignup'>
+      <Signup />
     </div>
   );
 }
